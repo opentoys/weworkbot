89369cc237fcb6116878f813c49b9bc6585425c0
xiaqb <xiaqiubo123@163.com>
2026-04-20 12:08:36 +0800

    feat: 优化命令调用

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module weworkbot
 
-go 1.21.0
+go 1.23
+
+toolchain go1.24.0
+
+require (
+	github.com/opentoys/wecombot v1.0.2
+	github.com/spf13/pflag v1.0.10
+)
--- main.go
+++ main.go
@@ -1,141 +1,168 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 	"time"
+
+	"github.com/opentoys/wecombot/webot"
+	"github.com/spf13/pflag"
 )
 
-const verison = "1.0.1"
-const help = `
-The commands are:
-    -h,-help       print help info 
-    -v,-verison    print verison
-    -k,-key        set wework bot key, support env X_WEWORK_BOT_KEY= for default
-    -f,-file       set upload and send file, 
-                        support local path,url, base64;example: ./a.zip, http://a.com/a.zip, data:base,sdjkashd=
-    -i,-image      set upload and send image, 
-                        support local path,url, base64;example: ./a.png, http://a.com/a.jpeg, data:base,sdjkashd=
-    -md,-markdown  send markdown message
-    -t,-text       send text message
-    -deadline      set deadline to cancel, support 1s, 2m3s...
-    -debug
+var version = "1.0.1"
 
-Wewrok bot doc: https://developer.work.weixin.qq.com/document/path/91770`
+var config = &struct {
+	Debug    bool
+	Version  bool
+	Type     string
+	Content  string
+	Key      string
+	File     string
+	Image    string
+	Markdown string
+	Text     string
+	Timeout  string
+}{}
 
-var key = ""
-var debug = false
-
-func println(args ...any) {
-	for _, v := range args {
-		fmt.Print(v)
-	}
-	fmt.Println("")
-}
-
-func checkprint(e error) {
-	if e != nil {
-		fmt.Println(e)
-		return
-	}
-	fmt.Println("success")
+func init() {
+	pflag.BoolVarP(&config.Debug, "debug", "d", false, "print debug message.")
+	pflag.StringVarP(&config.Key, "key", "k", os.Getenv("X_WEWORK_BOT_KEY"), "set wework bot key, support env X_WEWORK_BOT_KEY= for default")
+	pflag.StringVarP(&config.File, "file", "f", "", "set upload and send file, \r\nsupport local path,url, base64;example: ./a.zip, http://a.com/a.zip, data:base,sdjkashd=")
+	pflag.StringVarP(&config.Image, "image", "i", "", "set upload and send image, \r\nsupport local path,url, base64;example: ./a.png, http://a.com/a.jpeg, data:base,sdjkashd=")
+	pflag.StringVar(&config.Timeout, "timeout", "", "set deadline to cancel, support 1s, 2m3s...")
+	pflag.StringVarP(&config.Markdown, "markdown", "m", "", "send markdown v2 message")
+	pflag.StringVarP(&config.Text, "text", "t", "", "send text message")
 }
 
 func main() {
-	key = os.Getenv("X_WEWORK_BOT_KEY")
-	var data = ParseArgs(os.Args[1:])
-
-	if _, ok := data["debug"]; ok {
-		debug = true
+	if len(os.Args) == 1 {
+		printHelp()
+		return
 	}
-
-	if debug {
-		println(data)
+	switch os.Args[1] {
+	case "version", "v":
+		fmt.Printf("Usage of %s:\n", os.Args[0])
+		fmt.Println(version)
+		return
+	case "help", "h", "-help", "--help", "-h", "--h":
+		printHelp()
+		return
 	}
+	pflag.Parse()
 
-	if key != "" {
-		println("default key: ", key)
-	}
-
-	var ctx = context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-
-	var send struct {
-		typ     string
-		content string
-	}
-
-	for k, v := range data {
-		switch k {
-		case "k", "key":
-			key = v
-		case "v", "version":
-			fmt.Println(verison)
-			return
-		case "h", "help":
-			fmt.Println(help)
-			return
-		case "t", "text":
-			send.typ = "text"
-			send.content = v
-		case "md", "markdown":
-			send.typ = "markdown"
-			send.content = v
-		case "f", "file":
-			send.typ = "file"
-			send.content = v
-		case "i", "image":
-			send.typ = "image"
-			send.content = v
-		case "deadline":
-			du, e := time.ParseDuration(v)
-			if e == nil {
-				ctx, cancel = context.WithTimeout(ctx, du)
-			}
-		}
-	}
-	defer cancel()
-
-	if key == "" {
+	if config.Key == "" {
 		fmt.Println("key must be set")
-		fmt.Println(help)
+		printHelp()
 		return
 	}
 
-	if send.content == "" {
-		fmt.Println("send content must be set")
-		fmt.Println(help)
+	bot := webot.New(config.Key)
+
+	if config.Timeout != "" {
+		ts, e := time.ParseDuration(config.Timeout)
+		if e != nil {
+			panic(e)
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), ts)
+		defer cancel()
+		bot = bot.WithContext(ctx)
+	}
+
+	if config.Text != "" {
+		_, e := bot.SendText(config.Text)
+		if e != nil {
+			panic(e)
+		}
 		return
 	}
 
-	var bot = &Bot{ctx: ctx, key: key}
-	switch send.typ {
-	case "text":
-		fmt.Println("sending text messgae")
-		checkprint(bot.Text(send.content, "", ""))
-	case "markdown":
-		fmt.Println("sending markdown messgae")
-		checkprint(bot.Markdown(send.content))
-	case "image":
-		buf, _, e := Path2bytes(send.content)
+	if config.Markdown != "" {
+		_, e := bot.SendMarkdownV2(config.Markdown)
 		if e != nil {
-			fmt.Println(e)
-			return
+			panic(e)
 		}
-		fmt.Println("sending image")
-		checkprint(bot.Image(buf))
-	case "file":
-		buf, name, e := Path2bytes(send.content)
-		if e != nil {
-			fmt.Println(e)
-			return
-		}
-		if name == "" {
-			name = "unknow"
-		}
-		fmt.Println("sending file")
-		checkprint(bot.File(name, buf))
+		return
 	}
+
+	if config.File != "" {
+		buf, name, e := Path2bytes(config.File)
+		if e != nil {
+			panic(e)
+		}
+		resp, e := bot.UploadMedia(bytes.NewBuffer(buf), name, http.DetectContentType(buf))
+		if e != nil {
+			panic(e)
+		}
+		if !resp.IsOK() {
+			fmt.Println(resp.ErrMsg)
+		}
+		_, e = bot.SendFile(resp.MediaID)
+		if e != nil {
+			panic(e)
+		}
+		return
+	}
+
+	if config.Image != "" {
+		buf, _, e := Path2bytes(config.Image)
+		if e != nil {
+			panic(e)
+		}
+		_, e = bot.SendImage(base64.RawStdEncoding.EncodeToString(buf), hex.EncodeToString(md5.New().Sum(buf)))
+		if e != nil {
+			panic(e)
+		}
+		return
+	}
+}
+
+func printHelp() {
+	fmt.Printf("Usage of %s:\n\n", os.Args[0])
+	pflag.PrintDefaults()
+	fmt.Println("\r\n Wewrok bot doc: https://developer.work.weixin.qq.com/document/path/91770.")
+}
+
+func Path2bytes(s string) (buf []byte, name string, e error) {
+	if s == "" {
+		e = errors.New("input file error")
+		return
+	}
+	if strings.HasPrefix(s, "http") {
+		var resp *http.Response
+		resp, e = http.DefaultClient.Get(s)
+		if e != nil {
+			return nil, "", e
+		}
+		buf, e = io.ReadAll(resp.Body)
+		name = path.Base(resp.Request.URL.Path)
+		return
+	}
+
+	if strings.HasPrefix(s, "data:") {
+		var lst = strings.Split(s, ",")
+		if len(lst) == 0 {
+			e = errors.New("base64 error")
+			return
+		}
+		buf, e = base64.RawStdEncoding.DecodeString(lst[1])
+		if e != nil {
+			buf, e = base64.RawURLEncoding.DecodeString(lst[1])
+		}
+		return
+	}
+
+	buf, e = os.ReadFile(s)
+	name = filepath.Base(s)
+	return
 }
